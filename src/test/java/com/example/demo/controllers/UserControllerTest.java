6a905a40827d4ec37288b760155b0914c690f050
Rajashekar Chintalapati <rchintalapati@walmartlabs.com>
2019-09-30 00:40:49 -0700

    refactor: Adding test cases

---
+++ src/test/java/com/example/demo/controllers/UserControllerTest.java
@@ -0,0 +1,53 @@
+package com.example.demo.controllers;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.example.demo.TestUtils;
+import com.example.demo.model.persistence.User;
+import com.example.demo.model.persistence.repositories.CartRepository;
+import com.example.demo.model.persistence.repositories.UserRepository;
+import com.example.demo.model.requests.CreateUserRequest;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class UserControllerTest {
+
+    private UserController userController;
+    private UserRepository userRepository = mock(UserRepository.class);
+    private CartRepository cartRepository = mock(CartRepository.class);
+    private BCryptPasswordEncoder bCryptEncoder = mock(BCryptPasswordEncoder.class);
+
+    @Before
+    public void setup() {
+        userController = new UserController();
+        TestUtils.injectObjects(userController, "userRepository", userRepository);
+        TestUtils.injectObjects(userController, "cartRepository", cartRepository);
+        TestUtils.injectObjects(userController, "bCryptPasswordEncoder", bCryptEncoder);
+    }
+
+    @Test
+    public void createUserSuccess() {
+        when(bCryptEncoder.encode("password")).thenReturn("hashedpassword");
+        CreateUserRequest r = new CreateUserRequest();
+        r.setUsername("Test");
+        r.setPassword("password");
+        r.setConfirmPassword("password");
+
+        ResponseEntity<User> response = userController.createUser(r);
+        assertNotNull(response);
+        assertEquals(200, response.getStatusCodeValue());
+
+        User u = response.getBody();
+        assertNotNull(u);
+        assertEquals(0, u.getId());
+        assertEquals(u.getUsername(), r.getUsername());
+        assertEquals("hashedpassword", u.getPassword());
+    }
+
+}
